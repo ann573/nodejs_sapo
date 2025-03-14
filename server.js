8227596a8b34b43ec6323ad38bc4c50c6748f935
ann573 <ngthanhan0507@gmail.com>
2025-03-14 13:49:46 +0700

    fix cors

--- server.js
+++ server.js
@@ -10,32 +10,32 @@ dotenv.config();
 
 const { PORT, URL_FRONTEND } = process.env;
 
+// app.use(
+//   cors({
+//     origin: URL_FRONTEND, // Domain frontend
+//     credentials: true, // Cho phép gửi cookie
+//     allowedHeaders: ["Authorization", "Content-Type"],
+//     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
+//   })
+// );
 
-app.use(
-  cors({
-    origin: URL_FRONTEND, // Domain frontend
-    credentials: true, // Cho phép gửi cookie
-    allowedHeaders: ["Authorization", "Content-Type"],
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
-  })
-);
+app.use(cors({ origin: "*" }));
 
-app.use(cookieParser())
+app.use(cookieParser());
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
-
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", URL_FRONTEND || "*");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Authorization, Content-Type");
   res.header("Access-Control-Allow-Credentials", "true");
-  
+
   if (req.method === "OPTIONS") {
     return res.sendStatus(200);
   }
-  
+
   next();
 });
 
